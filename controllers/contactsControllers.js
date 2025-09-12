c2f6c85bfb820e72034fb4099a9ae646c7f66693
Darina Khalina <darina.khalina@namecheap.com>
2025-09-12 18:46:30 +0300

    feat: add hw 2 express

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,55 @@
-// import contactsService from "../services/contactsServices.js";
+import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    const result = await contactsService.listContacts();
+    res.status(200).json(result);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
 
-export const deleteContact = (req, res) => {};
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
 
-export const createContact = (req, res) => {};
+    res.status(200).json(result);
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(result);
+};
+
+export const createContact = async (req, res) => {
+    const result = await contactsService.addContact(req.body);
+
+    res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.updateContact(id, req.body);
+
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+
+    res.status(200).json(result);
+};
+
+export default {
+    getAllContacts: ctrlWrapper(getAllContacts),
+    getOneContact: ctrlWrapper(getOneContact),
+    deleteContact: ctrlWrapper(deleteContact),
+    createContact: ctrlWrapper(createContact),
+    updateContact: ctrlWrapper(updateContact),
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,22 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string()
+        .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+        .message("Phone number must be in the format (XXX) XXX-XXXX")
+        .required(),
+});
 
 export const updateContactSchema = Joi.object({
-
+    name: Joi.string().min(3).max(50),
+    email: Joi.string().email(),
+    phone: Joi.string()
+        .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+        .message("Phone number must be in the format (XXX) XXX-XXXX"),
 })
+    .min(1)
+    .messages({
+        "object.min": "Body must have at least one field",
+    });
