b0efebdfe7b167a26e55417c57113b31109f3a6a
Darina Khalina <darina.khalina@namecheap.com>
2025-09-12 18:58:46 +0300

    fix: upd log to error

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -9,7 +9,7 @@ async function listContacts() {
         const data = await fs.readFile(contactsPath, 'utf-8');
         return JSON.parse(data);
     } catch (error) {
-        console.log('Failed to read contacts file.', error.message);
+        console.error('Failed to read contacts file.', error.message);
         return [];
     }
 }
@@ -34,7 +34,7 @@ async function removeContact(contactId) {
         await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
         return removedContact;
     } catch (error) {
-        console.log('Failed to write contacts file.', error.message);
+        console.error('Failed to write contacts file.', error.message);
         return null;
     }
 }
@@ -50,7 +50,7 @@ async function addContact({ name, email, phone }) {
         await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
         return newContact;
     } catch (error) {
-        console.log("Failed to write contacts file.", error.message);
+        console.error("Failed to write contacts file.", error.message);
         return null;
     }
 }
